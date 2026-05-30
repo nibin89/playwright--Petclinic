c596b53b8a2a9760b15c6514e9a958a00b08dfc7
nibin89 <mathew.nibin89@gmail.com>
2026-05-30 12:56:30 -0700

    Completed Test case 1

--- tests/lists-and-dropdowns.spec.ts
+++ tests/lists-and-dropdowns.spec.ts
@@ -14,12 +14,19 @@ test('Validate selected Pet  types from the list', async ({ page }) => {
     await page.getByRole('link', { name: 'George Franklin' }).click()
     await expect(page.getByRole('heading', { name: 'Pets and Visits' })).toBeVisible()
     await page.getByRole('row').filter({ hasText: 'Leo' })
-    .getByRole('button', { name: 'Edit Pet' }).click();
-    await page.getByRole("heading", {name:" Pet "}).isVisible()
+        .getByRole('button', { name: 'Edit Pet' }).click();
+    await page.getByRole("heading", { name: " Pet " }).isVisible()
     await expect(page.locator("#owner_name")).toHaveValue("George Franklin")
     await expect(page.locator("#type1")).toHaveValue("cat")
 
-   const  typeDropdown = page.locator('#type');
-   const options = await typeDropdown.locator('option').allInnerTexts()
+    const typeDropdown = page.locator('#type');
+    const options = await typeDropdown.locator('option').allInnerTexts()
+    const typeTextBox = page.locator('#type1');
+
+    for (const option of options) {
+        await typeDropdown.selectOption(option)
+        await expect(typeTextBox).toHaveValue(option)
+
+    }
 
 });
