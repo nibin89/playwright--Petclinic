04d34be6ce88f96a1f57870517b6d53016a37d60
nibin89 <mathew.nibin89@gmail.com>
2026-06-01 07:26:02 -0700

    Test cases WIP progress

---
+++ tests/dialog-boxes.spec.ts
@@ -0,0 +1,20 @@
+import { test, expect } from '@playwright/test';
+
+test.beforeEach(async ({ page }) => {
+    await page.goto('/')
+
+})
+
+test('Add and Delete Pet Types', async ({ page }) => {
+    await page.getByRole("link", { name: "Pet Types" }).click();
+    await expect(page.getByRole("heading", { name: "Pet Types" })).toBeVisible()
+    await page.getByRole("button", { name: "Add" }).click()
+    await expect(page.getByRole("heading", { name: "New Pet Type" })).toBeVisible()
+    await expect(page.locator('label').filter({ hasText: 'Name' })).toBeVisible() //getByLabel("Name") won't work because the label is not properly associated with the input.
+    await expect(page.locator("#name")).toBeVisible() //since there are multiple textbox's in the page, id is used
+    await page.locator("#name").fill('pig')
+    await page.getByRole("button").filter({ hasText: 'Save' }).click()
+    await expect(page.getByRole('row').last().getByRole('textbox')).toHaveValue('pig')
+
+
+});
