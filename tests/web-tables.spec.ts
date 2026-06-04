83213273abfba9522d6ebc84c896a75a7d2cac06
nibin89 <mathew.nibin89@gmail.com>
2026-06-03 19:15:01 -0700

    Completed Pets of Madision city Test case

--- tests/web-tables.spec.ts
+++ tests/web-tables.spec.ts
@@ -5,6 +5,7 @@ test.beforeEach(async ({ page }) => {
   await page.getByRole('button', { name: 'Owners' }).click();
   await page.getByRole('link', { name: 'Search' }).click();
   await expect(page.getByRole('heading', { name: 'Owners' })).toBeVisible();
+  await expect(page.locator('tbody tr').first()).toBeVisible()
 });
 
 test('Validate the pet name city of the owner', async ({ page }) => {
@@ -55,11 +56,10 @@ test('Validate search by Last Name', async ({ page }) => {
   for (const row of await esRows.all()) {
     await expect(row.getByRole('cell').first()).toContainText('ES', { ignoreCase: true });
   }
-    await lastNameInput.clear();
-    await lastNameInput.fill('Playwright');
-    await findOwnerButton.click();
-    await expect(page.getByText('No owners with LastName starting with "Playwright"')).toBeVisible();
-  
+  await lastNameInput.clear();
+  await lastNameInput.fill('Playwright');
+  await findOwnerButton.click();
+  await expect(page.getByText('No owners with LastName starting with "Playwright"')).toBeVisible();
 });
 test('Validate phone number and pet name on the Owner Information page', async ({ page }) => {
   const ownerRow = page.locator('tbody tr', { hasText: '6085552765' });
@@ -67,21 +67,16 @@ test('Validate phone number and pet name on the Owner Information page', async (
   await expect(ownerRow.getByRole('cell').nth(3)).toHaveText('6085552765');
   await expect(petNameCell).toHaveText('George');
 });
-test.only('Validate pets of Madison city', async ({ page }) => {
+test('Validate pets of Madison city', async ({ page }) => {
   let pets = [];
-
-  const allRows = page.locator('tbody tr');
-
-  for (const row of await allRows.all()) {
+const ownerRows = page.locator('tbody tr').filter({ has: page.locator('td').nth(4) })
+  for (const row of await ownerRows.all()) {
     const cityCell = await row.locator('td').nth(2).textContent();
 
-    if (cityCell === 'Madison') {
+    if (cityCell?.trim() === 'Madison') {
       const petNameCell = await row.locator('td').nth(4).textContent();
-      pets.push(petNameCell)
-    }
-    else {
-        continue;
-    }
+      pets.push(petNameCell?.trim() || '');
+    } 
   }
 
   expect(pets).toEqual(['Leo', 'George', 'Mulligan', 'Freddy']);
