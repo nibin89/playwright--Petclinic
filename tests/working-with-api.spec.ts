3dd48441f0ee430a8b089ba69b6a72329d9ed6ed
nibin89 <mathew.nibin89@gmail.com>
2026-06-20 09:58:21 -0700

    Refactored the code based on PR feedback

--- tests/working-with-api.spec.ts
+++ tests/working-with-api.spec.ts
@@ -15,17 +15,14 @@ test.beforeEach(async ({ page }) => {
     });
   });
 
-
-  await page.route("*/**/petclinic/api/vets*", async (route) => {
+  await page.route('*/**/petclinic/api/vets*', async (route) => {
     const response = await route.fetch();
     const responseBody = await response.json();
 
     const vet = responseBody.find((v: any) => v.firstName === 'Sharon' && v.lastName === 'Jenkins');
-    
-    if(vet){
 
-vet.specialties = specialities
-
+    if (vet) {
+      vet.specialties = specialities;
     }
 
     await route.fulfill({ json: responseBody });
@@ -35,10 +32,10 @@ vet.specialties = specialities
 });
 
 test('validate ownerslist and visit list count', {
-  annotation: { 
-    type: 'description', 
-    description: 'Mocking API reponse.' 
-  },
+  annotation: {
+    type: 'description',
+    description: 'Mocking API reponse.'
+  }
 }, async ({ page }) => {
   await page.getByRole('button', { name: 'Owners' }).click();
   await page.getByRole('link', { name: 'Search' }).click();
@@ -51,7 +48,7 @@ test('validate ownerslist and visit list count', {
   const telephoneFromOwnersPage = (await ownerRows.first().locator('.ownerFullName + td + td + td').textContent())!;
   const petFromOwnersPage = (await ownerRows.first().locator('td').last().textContent())!;
   const petNamesFromOwnersPage = petFromOwnersPage.trim().split(/\s+/);
-  
+
   await ownerRows.getByRole('link').first().click();
   await expect(page).toHaveURL(`/owners/${owners[0].id}`);
 
@@ -73,26 +70,25 @@ test('validate ownerslist and visit list count', {
 
   const petNames = page.locator('app-pet-list dl');
   await expect(petNames).toHaveCount(owners[0].pets.length);
-  
+
   const firstPetSection = page.locator('app-pet-list').first();
-  const visitRows = firstPetSection.locator('app-visit-list table').getByRole('row').filter({ hasNotText: 'Visit Date' });
-  
+  const visitRows = firstPetSection.getByRole('table').getByRole('row').filter({ hasNotText: 'Visit Date' });
+
   await expect(visitRows).toHaveCount(10);
 }); 
 
-test('validate specilaities list for veterinarian',{
-  annotation: { 
-    type: 'description', 
-    description: 'Intercepting API reponse.' 
-  },
+test('validate specilaities list for veterinarian', {
+  annotation: {
+    type: 'description',
+    description: 'Intercepting API reponse.'
+  }
 }, async ({ page }) => {
   await page.getByRole('button', { name: 'Veterinarians' }).click();
   await page.getByRole('link', { name: 'All' }).click();
-  const sharonRow = page.getByRole('row').filter({ hasText: 'Sharon Jenkins' });
-  const specialtiesCell =  sharonRow.locator('td:has-text("Sharon Jenkins") + td')
+  const sharonRow = page.getByRole('row', { name: 'Sharon Jenkins' });
+  const specialtiesCell = sharonRow.locator('td:has-text("Sharon Jenkins") + td');
   for (let i = 0; i < specialities.length; i++) {
     await expect(specialtiesCell).toContainText(specialities[i].name);
   }
-  
 });
 
